560d841f1bd0fd3e35419e64e8f75e5ec27fc5d3
Daelyn Greene <dgreen41@vols.utk.edu>
2022-03-15 13:42:14 -0400

    made global and added things to ui

---
+++ global.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+
+library(dplyr)
+library(plotly)
+library(fpp3)
+library(xts)
+library(lubridate)
+library(quantmod)
+
---
+++ ui.R
@@ -0,0 +1,31 @@
+ui <- dashboardPage(
+  dashboardHeader("Daelyn Greene 475 Midterm"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("How to Use")
+    )
+  )
+  
+  
+  
+  
+  
+  # https://fred.stlouisfed.org/series/APU0000710411
+  # https://fred.stlouisfed.org/series/GASREGW
+  # 
+  # 
+  # 
+  # 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+)
