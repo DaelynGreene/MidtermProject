c29879e5f99af727f580d0200d018d951d6a67ad
Daelyn Greene <dgreen41@vols.utk.edu>
2022-03-21 13:50:31 -0400

    its been too long not sure

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 server <- function(input, output, session) {
   
-  input$the <- reactiveVal()
+  
   
 }
--- ui.R
+++ ui.R
@@ -1,37 +1,25 @@
 ui <- dashboardPage(
-  dashboardHeader("Daelyn Greene 475 Midterm"),
+  dashboardHeader(title - "Daelyn Greene 475 Midterm"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("How to Use",tabName = "Tab1")
+      menuItem("How to Use", tabName = "Tab1")
     )
   ),
-  
   dashboardBody(
-   tabItem(tabName = "Tab1",
-                     actionButton("the","the")
+    tabItem(
+      tabName = "Tab1",
+      actionButton("the", "the")
     )
   )
-  
-  
-  
-  
-  
+
+
+
+
+
   # https://fred.stlouisfed.org/series/APU0000710411
   # https://fred.stlouisfed.org/series/GASREGW
-  # 
-  # 
-  # 
-  # 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  #
+  #
+  #
+  #
 )
